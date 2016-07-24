b03375f130e4581885c483d1dd35b943ff3ae0b8
Anthony DiMarco <ocramida@gmail.com>
2016-07-24 16:31:26 -0700

    add base for s3 integration tests

---
+++ tests/s3.rs
@@ -0,0 +1,20 @@
+#![cfg(feature = "s3")]
+extern crate env_logger;
+extern crate rusoto;
+
+#[macro_use] 
+extern crate log;
+
+
+use rusoto::s3::S3Client;
+use rusoto::{DefaultCredentialsProvider, Region};
+
+#[test]
+fn should_list_buckets() {
+	let _ = env_logger::init();
+    let credentials = DefaultCredentialsProvider::new().unwrap();
+    let client = S3Client::new(credentials, Region::UsEast1);
+
+	let result = client.list_buckets().unwrap();
+    debug!("{:#?}", result);
+}
